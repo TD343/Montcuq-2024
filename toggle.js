c32039f93644e8b88cfefb59975adf684c82965c
TD343 <tomdbrault@gmail.com>
2024-12-19 10:40:21 -0500

    commit chnaghes

---
+++ toggle.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+    toggleButton.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  });
+</script>
